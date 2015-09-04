44948337101e8177c3c9abc97da9ec88fe65b256
winsxx <13512071@std.stei.itb.ac.id>
2015-09-04 23:22:00 +0700

    Ensemble learning with random forest

---
+++ learning_random_forest.R
@@ -0,0 +1,40 @@
+# Taxi Trajectory, By LapanDua
+
+#####
+# Depedency
+#####
+source(feature.R)
+library(randomForest)
+
+#####
+# Script to Run
+#####
+set.seed(0)
+
+trees.amount <- 100
+
+class.index <- ncol(train.feature)
+rf <- randomForest(x = train.feature[,-class.index],
+                   y = droplevels(train.feature[,class.index]),
+                   ntree = trees.amount,
+                   do.trace = T)
+
+prediction <- predict(rf, test.feature)
+
+predicted.coords.list <-lapply(as.character(prediction), classToCoordinate)
+predicted.coords <- do.call("rbind",predicted.coords.list)
+predicted.coords <- as.data.frame(predicted.coords)
+
+options(digits=9)
+submission <- cbind(TRIP_ID = test[,1],
+                    LATITUDE = predicted.coords[,2],
+                    LONGITUDE = predicted.coords[,1])
+submission <- as.data.frame(submission)
+submission$LATITUDE <- format(round(as.numeric(as.character(submission$LATITUDE)),6), nsmall=6)
+submission$LONGITUDE <- format(round(as.numeric(as.character(submission$LONGITUDE)),6), nsmall=6)
+
+write_csv(submission, "output/submission.csv") 
+
+if(TEST_OWN){
+  destinationMining.Evaluation("output/submission.csv", "output/test_own.csv")
+}
